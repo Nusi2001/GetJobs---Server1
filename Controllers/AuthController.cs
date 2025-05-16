b494106f759299d7611fb6144265a815a56f2bb8
Nusi Siri Chandana <sirinvnr14@gmail.com>
2025-05-16 02:01:37 -0700

    Changed AuthController route to /api/login

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -10,7 +10,7 @@ using System.Text;
 namespace Server.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api")]
 public class AuthController : ControllerBase
 {
     private readonly UserManager<ApplicationUser> _userManager;
